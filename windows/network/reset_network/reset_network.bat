042fb3654eb51f0d84ea166a988bbb08714c60aa
unsreg <unsreg@gmail.com>
2020-07-29 14:51:22 +0300

    Added cache reset

--- windows/network/reset_network/reset_network.bat
+++ windows/network/reset_network/reset_network.bat
@@ -8,5 +8,6 @@ netsh int tcp set heuristics disabled
 netsh int tcp set global autotuninglevel=disable
 netsh int tcp set global rss=enabled
 netsh int tcp show global
+netsh int ip delete arpcache
 
 pause
