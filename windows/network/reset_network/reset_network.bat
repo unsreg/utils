b08d36e2de4e3bd98ad6c220561d365e207b57ba
unsreg <unsreg@gmail.com>
2020-07-30 18:08:27 +0300

    added comment

--- windows/network/reset_network/reset_network.bat
+++ windows/network/reset_network/reset_network.bat
@@ -8,6 +8,7 @@ netsh int tcp set heuristics disabled
 netsh int tcp set global autotuninglevel=disable
 netsh int tcp set global rss=enabled
 netsh int tcp show global
+::https://answers.microsoft.com/ru-ru/windows/forum/all/%D1%81%D0%BA%D0%B0%D1%87%D0%B5%D1%82/1fab45e9-6a84-459a-9c72-c83048cedf60?auth=1
 netsh int ip delete arpcache
 
 pause
